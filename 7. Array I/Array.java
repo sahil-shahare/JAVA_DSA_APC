625d88654f945402c8cfd358085d11248d8c55df
sahil-shahare <sahilshahare380@gmail.com>
2026-06-26 22:18:48 +0530

    Array

--- 7. Array I/Array.java
+++ 7. Array I/Array.java
@@ -193,7 +193,7 @@ public class Array {
         // maxSubarraySum(number);
         // kadanes(number);
 
-        int[] height = { 4, 2, 0, 6, 3, 2, 5, 6};
+        int[] height = { 4, 2, 0, 6, 3, 2, 5};
         System.out.println(trappedWater(height));
 
     }
